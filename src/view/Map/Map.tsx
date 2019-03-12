fa592af39f0c9b8a5a02edc598d02ce631162f55
Pavol Hejny <me@pavolhejny.com>
2019-03-12 18:08:32 +0100

    Multiple polygons

--- src/view/Map/Map.tsx
+++ src/view/Map/Map.tsx
@@ -8,57 +8,89 @@ interface IMapProps {
     appState: IAppState & IObservableObject;
 }
 
-type IMapBoundaries = [number|null,number|null,number|null,number|null]
+type IMapBoundaries = [
+    number | null,
+    number | null,
+    number | null,
+    number | null
+];
 
 export const Map = observer(({ appState }: IMapProps) => {
-
-    if(!appState.opened){
-        return  <>Opening</>;
+    if (!appState.opened) {
+        return <>Opening</>;
     }
 
-
-    const coordinatesWGS84 = appState.opened.features[0].geometry.coordinates[0];
+    const coordinatesWGS84 =
+        appState.opened.features[0].geometry.coordinates[0];
+    const coordinatesWGS84All = appState.opened.features.reduce(
+        (coords, feature) => [...coords, ...feature.geometry.coordinates[0]],
+        [] as [number, number][],
+    );
 
     console.log(coordinatesWGS84);
 
-    const boundaries = 
-    coordinatesWGS84.reduce((boundaries,coords)=>{
+    const boundaries = coordinatesWGS84All.reduce(
+        (boundaries, coords) => {
+            let [latMin, lngMin, latMax, lngMax] = boundaries;
+            let [lat, lng] = coords; //todo is it in the correct order
 
-        let [latMin,lngMin, latMax,lngMax] = boundaries;
-        let [lat,lng] = coords;// as any as [number,number];
-     
-        if(!latMin || latMin>lat)latMin=lat;
-        if(!lngMin || lngMin>lng)lngMin=lng;
-        if(!latMax || latMax<lat)latMax=lat;
-        if(!lngMax || lngMax<lng)lngMax=lng;
+            if (!latMin || latMin > lat) latMin = lat;
+            if (!lngMin || lngMin > lng) lngMin = lng;
+            if (!latMax || latMax < lat) latMax = lat;
+            if (!lngMax || lngMax < lng) lngMax = lng;
 
+            return [latMin, lngMin, latMax, lngMax] as IMapBoundaries;
+        },
+        [null, null, null, null] as IMapBoundaries,
+    );
 
-        return [latMin,lngMin, latMax,lngMax] as IMapBoundaries;
+    const width=800,height=500;
 
-    },[null,null,null,null] as IMapBoundaries);
-
-    console.log('boundaries',boundaries);
-
-    
-    const coordinatesRelative = coordinatesWGS84.map(([lat,lng])=>[
-        toRelativeBoundaries(lat,boundaries[0]!,boundaries[2]!),
-        toRelativeBoundaries(lng,boundaries[1]!,boundaries[3]!)
-    ])
-
-    console.log(coordinatesRelative);
-    
     return (
         <div className="Map">
-            {!appState.opened ? <>Opening</> : <>
-                test
-                <svg height="500" width="500">
-                    <polygon points={coordinatesRelative.map(([lat,lng])=>`${lat*500},${lng*500}`).join(' ')} style={{fill:"lime",stroke:"purple",strokeWidth:1}} />
-                </svg> 
-            </>}
+            {!appState.opened ? (
+                <>Opening</>
+            ) : (
+                <>
+                    <svg
+                        {...{width,height}}
+                        style={{ border: '2px solid red' }}
+                    >
+                        {appState.opened.features.map((feature) => (
+                            <polygon
+                                points={feature.geometry.coordinates[0]
+                                    .map(([lat, lng]) => [
+                                        1 -
+                                            toRelativeBoundaries(
+                                                lat,
+                                                boundaries[0]!,
+                                                boundaries[2]!,
+                                            ),
+                                        toRelativeBoundaries(
+                                            lng,
+                                            boundaries[1]!,
+                                            boundaries[3]!,
+                                        ),
+                                    ])
+                                    .map(
+                                        ([lat, lng]) =>
+                                            `${lat * width},${lng * height}`,
+                                    )
+                                    .join(' ')}
+                                style={{
+                                    fill: 'lime',
+                                    stroke: 'purple',
+                                    strokeWidth: 1,
+                                }}
+                            />
+                        ))}
+                    </svg>
+                </>
+            )}
         </div>
     );
 });
 
-function toRelativeBoundaries(value: number, max:number, min: number):number{
-    return (value-min)/(max-min);
+function toRelativeBoundaries(value: number, max: number, min: number): number {
+    return (value - min) / (max - min);
 }
